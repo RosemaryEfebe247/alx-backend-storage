b18d94ed2fc9da77e1708d91d5577c37c1eaccf1
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-12 03:08:01 +0100

    country_users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- Creates a table with attributes and a country attribute with enum val
-CREATE TABLE IF NOT EXIST users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
