a346c08dd60ae55c3c34d4f06b3a1f76f07800e7
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-12 04:12:19 +0100

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Rank country origins of bands by the number of fans
 SELECT origin, SUM(fans) AS nb_fans
-FROM meta_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+	FROM meta_bands
+	GROUP BY origin
+	ORDER BY nb_fans DESC;
