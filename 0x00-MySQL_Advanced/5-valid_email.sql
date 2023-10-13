9d81a12a243aa1755e1c2478ef6956ac9d8ec69b
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-13 12:46:40 +0100

    valid_email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- Update valid email when email has been changed
+DELIMITER //
+CREATE TRIGGER update_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email <> OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END; //
+DELIMITER ;
